9e937e012f752ba01936826e88b2adc927bf688f
zbergue-cpu <zbergue@gmail.com>
2026-09-09 15:21:01 +0200

    Update to v2.0

--- myapp/app.py
+++ myapp/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ArgoCD v1.0!"
+    return "Hello from ArgoCD v2.0!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
